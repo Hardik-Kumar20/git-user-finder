273db7d3087ef5defeeb980629d2f0e80c643555
Hardik Kumar <kumarhardik821@gmail.com>
2025-01-12 09:38:48 -0800

    add a new tilt.js file for slight tilt effect and working on gitSearch.js

---
+++ gitSearch.js
@@ -0,0 +1,19 @@
+let api = "https://api.github.com/users/";
+let fetch = document.createElement("script");
+fetch.src = ;
+fetch.integrity = ;
+
+fetch.crossOrigin = "anonymous";
+document.head.appendChild(fetch);
+
+let search = document.querySelector(".input").value;
+let avatar = document.querySelector(".profile-photo");
+let user = document.querySelector(".userName");
+let followers = document.querySelector(".followers");
+let following = document.querySelector(".following");
+let repos = document.querySelector(".repos");
+
+
+const userGetFunction = (name) =>{
+    axios
+}
